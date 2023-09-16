5a331ef95f5a98c9a7e4d93316348d5d1c23c53b
sidy74 <snagatod74@gmail.com>
2023-09-16 10:46:16 +0000

    add component appbar

---
+++ src/app/appbar/appbar.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppbarComponent } from './appbar/appbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppbarModule { }
