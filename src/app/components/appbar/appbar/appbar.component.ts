07ad933356b3958d987abc5aa687d78075d9da35
sidy74 <snagatod74@gmail.com>
2023-09-17 20:41:25 +0000

    appbar content and amination when scroll

---
+++ src/app/components/appbar/appbar/appbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-appbar',
+  templateUrl: './appbar.component.html',
+  styleUrls: ['./appbar.component.css'],
+})
+export class AppbarComponent {
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    console.log(document.body.scrollTop);
+    console.log(document.documentElement.scrollTop);
+
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      document.getElementById('toolbar')!.style.padding = '30px 10px';
+      document.getElementById('logo')!.style.height = '100px';
+      document.getElementById('logo')!.style.width = '100px';
+    } else {
+      document.getElementById('toolbar')!.style.padding = '80px 10px';
+      document.getElementById('logo')!.style.height = "200px";
+      document.getElementById('logo')!.style.width = "200px";
+    }
+   
+  }
+}
