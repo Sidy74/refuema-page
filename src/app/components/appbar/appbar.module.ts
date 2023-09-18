321cab2a0db12ad04b859754e175efc75791c10f
sidy74 <snagatod74@gmail.com>
2023-09-18 23:40:01 +0000

    add route links in template & approuting

--- src/app/components/appbar/appbar.module.ts
+++ src/app/components/appbar/appbar.module.ts
@@ -4,6 +4,7 @@ import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatButtonModule} from '@angular/material/button';
 import{MatIconModule} from '@angular/material/icon'
 import { AppbarComponent } from './appbar/appbar.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { AppbarComponent } from './appbar/appbar.component';
   imports: [
     CommonModule,MatToolbarModule,
     MatButtonModule,
+    RouterModule,
     MatIconModule
   ],
   exports:[AppbarComponent]
