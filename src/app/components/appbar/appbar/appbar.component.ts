7114ca9797db827f1710cc57c2f358792a1c7842
sidy74 <snagatod74@gmail.com>
2023-09-19 23:45:18 +0000

    Add mono color, add first section in home component & update style of appbar

--- src/app/components/appbar/appbar/appbar.component.ts
+++ src/app/components/appbar/appbar/appbar.component.ts
@@ -12,22 +12,17 @@ export class AppbarComponent {
       document.body.scrollTop > 10 ||
       document.documentElement.scrollTop > 20
     ) {
-      
-      document.getElementById('navbar')!.style.padding='30px 0px'; 
+      document.getElementById('navbar')!.style.padding = '30px 0px';
       document.getElementById('toolbar')!.style.padding = '30px 10px';
       document.getElementById('logo')!.style.height = '100px';
       document.getElementById('logo')!.style.width = '100px';
-      document.getElementById('toolbar')!.style.opacity ='0.91'; 
-     console.log( document.documentElement.scrollTop);
-      console.log( document.getElementById('navbar')!.style.padding);
-     
+      document.getElementById('toolbar')!.style.opacity = '0.91';
     } else {
-      document.getElementById('navbar')!.style.padding='90px 0px'; 
+      document.getElementById('navbar')!.style.padding = '90px 0px';
       document.getElementById('toolbar')!.style.padding = '90px 10px';
       document.getElementById('logo')!.style.height = '200px';
       document.getElementById('logo')!.style.width = '200px';
-      document.getElementById('toolbar')!.style.opacity ='1';
-      console.log( document.getElementById('navbar')!.style.padding);
+      document.getElementById('toolbar')!.style.opacity = '1';
     }
   }
 }
