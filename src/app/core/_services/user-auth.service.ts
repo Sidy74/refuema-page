1c45072b5a7e21631b28a7c9a7664da855e77643
sidy74 <snagatod74@gmail.com>
2023-10-24 13:31:57 +0000

    add userToken service

--- src/app/core/_services/user-auth.service.ts
+++ src/app/core/_services/user-auth.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UserAuthService {
-  isUserAuthService = true;
+  isUserAuthService = false;
   constructor() {}
 }
