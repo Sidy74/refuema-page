df2e52bd822e3ce991ece3177184e762c6a7b8ee
sidy74 <snagatod74@gmail.com>
2023-12-11 18:16:40 +0000

    reponsive appbar on mobile device

--- src/app/components/appbar/appbar-avatar/appbar-avatar.component.ts
+++ src/app/components/appbar/appbar-avatar/appbar-avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { LoadingService } from 'src/app/core/_services/loading.service';
 import { LoginService } from 'src/app/core/_services/login.service';
@@ -9,12 +9,15 @@ import { ToastService } from 'src/app/core/_services/toast/toast.service';
   templateUrl: './appbar-avatar.component.html',
   styleUrls: ['./appbar-avatar.component.css'],
 })
-export class AppbarAvatarComponent {
+export class AppbarAvatarComponent implements OnInit {
   constructor(
     private userAuthService: LoginService,
     private toastService: ToastService,
     private loadingService: LoadingService
   ) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
   @ViewChild(MatMenuTrigger)
   trigger!: MatMenuTrigger;
@@ -27,19 +30,21 @@ export class AppbarAvatarComponent {
   @HostListener('window:scroll', ['$event'])
   onScroll(event: any) {
     this.closeMyMenu();
-    if (
-      document.body.scrollTop > 10 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      document.getElementById('image-container')!.style.height = '40px';
-      document.getElementById('image-container')!.style.width = '40px';
-      document.getElementById('profil-image')!.style.width = '40px';
-      document.getElementById('profil-image')!.style.height = '40px';
-    } else {
-      document.getElementById('image-container')!.style.height = '50px';
-      document.getElementById('image-container')!.style.width = '50px';
-      document.getElementById('profil-image')!.style.width = '50px';
-      document.getElementById('profil-image')!.style.height = '50px';
+    if (screen.width > 600) {
+      if (
+        document.body.scrollTop > 10 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        document.getElementById('image-container')!.style.height = '40px';
+        document.getElementById('image-container')!.style.width = '40px';
+        document.getElementById('profil-image')!.style.width = '40px';
+        document.getElementById('profil-image')!.style.height = '40px';
+      } else {
+        document.getElementById('image-container')!.style.height = '50px';
+        document.getElementById('image-container')!.style.width = '50px';
+        document.getElementById('profil-image')!.style.width = '50px';
+        document.getElementById('profil-image')!.style.height = '50px';
+      }
     }
   }
   logout() {
--- src/app/components/appbar/appbar/appbar.component.ts
+++ src/app/components/appbar/appbar/appbar.component.ts
@@ -11,7 +11,10 @@ export class AppbarComponent implements OnInit, OnDestroy {
   loginSubscription?: Subscription;
   isAuthentified!: boolean;
   ErroStatus!: string;
+  isMobileDevice!: boolean;
   isOpen: boolean = false;
+  numClick = 0;
+
   constructor(private loginService: LoginService) {
     this.loginSubscription = this.loginService.isLogged().subscribe({
       next: (value) => {
@@ -20,41 +23,75 @@ export class AppbarComponent implements OnInit, OnDestroy {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (screen.width <= 600) {
+      this.isMobileDevice = true;
+    }
+  }
 
   logout() {
     this.loginService.logout();
   }
 
+  @HostListener('window:click', ['$event'])
+  onclick(event: any) {
+    const navbar_center_mobile = document.getElementById('menu-items-mobile');
+
+    if (navbar_center_mobile) {
+      if (navbar_center_mobile.contains(event.target)) return;
+
+      if (!navbar_center_mobile.contains(event.target) && this.numClick > 0) {
+        this.isOpen = false;
+        this.numClick = 0;
+        return;
+      }
+      if (
+        !navbar_center_mobile.isEqualNode(event.target) &&
+        this.numClick > 0
+      ) {
+        this.isOpen = false;
+        this.numClick = 0;
+        return;
+      }
+      if (this.isOpen) this.numClick++;
+    }
+  }
+
   @HostListener('window:scroll', ['$event'])
   onScroll(event: any) {
-    if (
-      document.body.scrollTop > 10 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      document.getElementById('navbar')!.style.padding = '30px 0px';
-      document.getElementById('toolbar')!.style.padding = '30px 10px';
-      document.getElementById('logo')!.style.height = '100px';
-      document.getElementById('logo')!.style.width = '100px';
-      document.getElementById('toolbar')!.style.opacity = '0.91';
-    } else {
-      document.getElementById('navbar')!.style.padding = '90px 0px';
-      document.getElementById('toolbar')!.style.padding = '90px 10px';
-      document.getElementById('logo')!.style.height = '200px';
-      document.getElementById('logo')!.style.width = '200px';
-      document.getElementById('toolbar')!.style.opacity = '1';
+    if (!this.isMobileDevice) {
+      if (
+        document.body.scrollTop > 10 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        document.getElementById('navbar')!.style.padding = '30px 0px';
+        document.getElementById('toolbar')!.style.padding = '30px 10px';
+        document.getElementById('logo')!.style.height = '100px';
+        document.getElementById('logo')!.style.width = '100px';
+        document.getElementById('toolbar')!.style.opacity = '0.86';
+        document.getElementById('fixed-div')!.style.opacity = '0.86';
+      } else {
+        document.getElementById('navbar')!.style.padding = '90px 0px';
+        document.getElementById('toolbar')!.style.padding = '90px 10px';
+        document.getElementById('logo')!.style.height = '200px';
+        document.getElementById('logo')!.style.width = '200px';
+        document.getElementById('toolbar')!.style.opacity = '1';
+      }
     }
+  }
+
+  closeMenuHamburger() {
+    this.isOpen = false;
   }
   clickMenuHamburger() {
     var x = document.getElementById('menuItems');
     if (x?.className === 'navbar-center') {
       this.isOpen = true;
-      x.className += ' responsive';
     } else {
-      x!.className = 'navbar-center';
       this.isOpen = false;
     }
   }
+
   ngOnDestroy(): void {
     this.loginSubscription?.unsubscribe();
   }
