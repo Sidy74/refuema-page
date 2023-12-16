42efeb51a7b95f2670d9d62216e96cd19d7d82dc
sidy74 <snagatod74@gmail.com>
2023-12-16 20:09:40 +0000

    Implementing future for modifying password of the authenticated user

--- src/app/components/user/profil/edit-user-password/edit-user-password.component.ts
+++ src/app/components/user/profil/edit-user-password/edit-user-password.component.ts
@@ -1,5 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { LoadingService } from 'src/app/core/_services/loading.service';
+import { ToastService } from 'src/app/core/_services/toast/toast.service';
+import { UserService } from 'src/app/core/_services/user/user.service';
 import { PasswordValidator } from 'src/app/core/_validator/password.validator';
 
 @Component({
@@ -10,7 +15,17 @@ import { PasswordValidator } from 'src/app/core/_validator/password.validator';
 export class EditUserPasswordComponent implements OnInit {
   editPasswordForm!: FormGroup;
   formControls!: any;
-  constructor(private fb: FormBuilder) {}
+  isLoading: boolean = false;
+  HttpErrorMessage: any = null;
+  hasChange = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private toastService: ToastService,
+    public loadingService: LoadingService,
+    public dialogRef: MatDialogRef<EditUserPasswordComponent>
+  ) {}
   ngOnInit(): void {
     this.editPasswordForm = this.fb.group({
       oldPassword: ['', Validators.required],
@@ -23,8 +38,51 @@ export class EditUserPasswordComponent implements OnInit {
       ),
     });
     this.formControls = this.editPasswordForm.controls;
+
+    this.onEditPasswordFormValueChange();
   }
+
   updatePassword(event: any) {
-    console.log(this.editPasswordForm);
+    const passwords = this.passwordsToFormData();
+    this.userService.resteAuthenticatedUserPassword(passwords).subscribe({
+      next: (value: any) => {
+        console.log(value);
+        this.toastService.openSuccess(value.message, 'X');
+        this.closeDialog(true);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+        if (err.status == 401) {
+          err.message;
+          this.HttpErrorMessage = err.message;
+        }
+        this.toastService.openError('Mot de passe incorrecte', 'X');
+        this.loadingService.isLoading.next(false);
+      },
+      complete: () => this.loadingService.isLoading.next(false),
+    });
+  }
+  passwordsToFormData() {
+    let formData = new FormData();
+    formData.append(' old_password', this.formControls['oldPassword'].value);
+    formData.append(
+      'password',
+      this.formControls['pwrdForm'].controls['password'].value
+    );
+    return formData;
+  }
+
+  onEditPasswordFormValueChange() {
+    const initialValue = this.editPasswordForm.value;
+    this.editPasswordForm.valueChanges.subscribe((value) => {
+      this.hasChange = Object.keys(initialValue).some((key) => {
+        this.editPasswordForm.value[key] != initialValue[key];
+        this.HttpErrorMessage = null;
+      });
+    });
+  }
+
+  closeDialog(data: any) {
+    this.dialogRef.close(data);
   }
 }
