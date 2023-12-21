3ce004b0b7361b4ca707415c52fd275f39e1fc8f
sidy74 <snagatod74@gmail.com>
2023-12-21 12:39:29 +0000

    update card

--- src/app/components/user/card/card.component.ts
+++ src/app/components/user/card/card.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { CardService } from 'src/app/core/_services/card/card.service';
+import { ImageService } from 'src/app/core/_services/images/image.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
+  isgeneradedCard: boolean = false;
+  cardMatricule: string = 'SIdyMATRICLE';
+  cardDateFin: string = '23000';
+  cardDateDebut: string = '23000';
+  cardTitre: string = 'PR';
+  cardImage :string=''
+  cardUserFistName: string = 'Fatoumata Mohamed Ag';
+  cardUserLastName: string = 'Coulibaly';
+  cardSpecialite: string = 'Ingénieur Maintenance Réseau infomratique Mali';
 
+  constructor(private cardService: CardService , private imageService :ImageService) {
+    this.getCard();
+  }
+  setCardInfomations(user: any) {
+    this.cardMatricule = user.numero_matricule;
+    this.cardDateFin = user.fin;
+    this.cardDateDebut = user.debut;
+    this.cardTitre = user.titre_utilisateur;
+    this.cardUserFistName = user.prenom;
+    this.cardUserLastName = user.nom;
+    this.cardSpecialite = user.specialite;
+    this.cardImage = this.imageService.getImageUrl(user.photo)
+  }
+  getCard() {
+    this.cardService.getCard().subscribe({
+      next: (value) => {
+        console.log(value);
+        this.isgeneradedCard = true;
+        this.setCardInfomations(value)
+      },
+      error(err) {
+        console.log(err);
+      },
+    });
+  }
+  generateCard() {
+    this.cardService.generateCard().subscribe({
+      next: (value) => {
+        console.log(value);
+        this.getCard();
+      },
+      error(err) {
+        console.log(err);
+      },
+    });
+  }
 }
