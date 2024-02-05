46f621e5402486a461a1847306a404271ef63d80
sidy74 <snagatod74@gmail.com>
2024-02-05 20:22:44 +0000

    table of articles & create add article component

---
+++ src/app/components/admin/a-articles/a-add-article/a-add-article.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-a-add-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './a-add-article.component.html',
+  styleUrl: './a-add-article.component.css'
+})
+export class AAddArticleComponent {
+
+}
